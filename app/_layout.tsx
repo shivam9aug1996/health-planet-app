ae9125a9f031262699d9dbd4ace5e71b7685c761
shivam-appboxer <shivam@appboxer.com.au>
2024-06-06 14:23:48 +0530

    fixed bug

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,12 +13,18 @@ import store from "@/redux/store";
 import { Provider } from "react-redux";
 import MainNavigator from "@/components/MainNavigator";
 import ToastManager from "toastify-react-native";
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const insets = useSafeAreaInsets();
+
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -34,12 +40,14 @@ export default function RootLayout() {
   }
 
   return (
-    <Provider store={store}>
-      <ToastManager />
+    <SafeAreaProvider style={{ paddingTop: insets.top }}>
+      <Provider store={store}>
+        <ToastManager />
 
-      <ThemeProvider value={DefaultTheme}>
-        <MainNavigator />
-      </ThemeProvider>
-    </Provider>
+        <ThemeProvider value={DefaultTheme}>
+          <MainNavigator />
+        </ThemeProvider>
+      </Provider>
+    </SafeAreaProvider>
   );
 }
